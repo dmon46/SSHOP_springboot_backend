8292943d43127d04c874aae6cbee0c95cef7a93f
tn9th8 <tn9th8@gmail.com>
2025-01-27 10:27:03 +0700

    f: identity: change to account, shop model

--- _domain/src/main/java/com/dmon/sshop/_domain/identity/repository/IUserDomainRepository.java
+++
@@ -1,8 +0,0 @@
-package com.dmon.sshop._domain.identity.repository;
-
-import com.dmon.sshop._domain.identity.model.entity.User;
-
-public interface IUserDomainRepository {
-    // PERSIST//
-    User save(User user);
-}
--- _infrastructure/src/main/java/com/dmon/sshop/_infrastructure/persistence/jpa/identity/IUserJpaMapper.java
+++
@@ -1,7 +0,0 @@
-package com.dmon.sshop._infrastructure.persistence.jpa.identity;
-
-import com.dmon.sshop._domain.identity.model.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IUserJpaMapper extends JpaRepository<User, String> {
-}
